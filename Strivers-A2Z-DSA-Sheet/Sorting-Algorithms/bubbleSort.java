4933fb266cbd77aa2b35d27e18b4a0e2995292f5
Naveen Kumar D <87007525+NaveenKumarNlp@users.noreply.github.com>
2024-04-02 12:37:30 +0530

    Create bubbleSort.java

---
+++ Strivers-A2Z-DSA-Sheet/Sorting-Algorithms/bubbleSort.java
@@ -0,0 +1,29 @@
+package StriverDSASheet;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sc.close();
+
+        for (int i = n-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+//TC -- O(N*N)
+//SC -- O(1)
