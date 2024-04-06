0711140e0a7ffba7adeacadd5d65f1f3193f7b1e
Naveen Kumar D <87007525+NaveenKumarNlp@users.noreply.github.com>
2024-04-06 11:28:18 +0530

    Add files via upload

---
+++ Strivers-A2Z-DSA-Sheet/Arrays/rightRotateTheArrayByDPlace.java
@@ -0,0 +1,33 @@
+package StriverDSASheet;
+
+import java.util.Scanner;
+
+public class rightRotateTheArrayByDPlace {
+    public static void revrse(int []arr,int left,int right){
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        sc.close();
+        revrse(arr, n-x, n-1);
+        revrse(arr, 0, x);
+        revrse(arr, 0, n-1);
+        
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
